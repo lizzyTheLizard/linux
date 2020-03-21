dc9373d622bcab1fbaee9edccdc25505555e1f63
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2020-03-21 17:00:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
 echo "Hello World"
+
+#Update the actual system
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+
+#Ensure needed software is installed
+sudo apt-get install vim git
+
+
+#software:
+postman
+visual studio code
+intellij
+most recent java
+
+
+#config
+vim
+no login
+keyboard
+bash
+alias
+
+
+
+
