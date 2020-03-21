c97e69ba72996ae0772a4585a0ff88473e0deb1b
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2020-03-21 17:11:37 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,10 @@ sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 #Ensure needed software is installed
 sudo apt-get install vim 
 
-echo $0
-echo $1
+
+
+echo "$0"
+echo "$1"
 
 if [ "$1" == "desktop" ]
 then
