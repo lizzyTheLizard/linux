0d9a62287b11b2de1e9befdd68feb3b07c4852f9
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2020-03-21 17:07:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 #Ensure needed software is installed
 sudo apt-get install vim 
 
-if [ $1 -eq 'desktop' ] then
+if [ $1 -eq 'desktop' ]
+then
   sudo apt install gnome-session gnome-terminal 
 fi
